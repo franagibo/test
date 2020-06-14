c67a1370a1d5a4c237f571dab38edaacdeadc62c
franagrana <francesco@giberti.org>
2020-06-14 22:52:46 +0200

    0

--- src/main/java/com/frana/taskme/repositories/UserRepository.java
+++ src/main/java/com/frana/taskme/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.frana.taskme.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 import com.frana.taskme.entities.User;
